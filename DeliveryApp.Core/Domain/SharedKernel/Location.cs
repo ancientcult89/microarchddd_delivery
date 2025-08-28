52aa69bf8060ed2f1d7ca8ddddfdbe0d431fcdf5
SP_Polyakov <sp_polyakov@dba.ooo>
2025-08-28 21:11:46 +0700

    ДЗ 2

---
+++ DeliveryApp.Core/Domain/SharedKernel/Location.cs
@@ -0,0 +1,73 @@
+﻿using CSharpFunctionalExtensions;
+using Primitives;
+
+namespace DeliveryApp.Core.Domain.SharedKernel
+{
+    public class Location : ValueObject
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        private static readonly Random _random = new Random();
+
+        private Location() { }
+
+        public Location(int x, int y) : this()
+        {
+            try
+            {
+                SetX(x);
+                SetY(y);
+            }
+            catch(ArgumentException e)
+            {
+                throw new ArgumentException(e.Message);
+            }
+        }
+
+        public void SetX(int x)
+        {
+            if (x < 1)
+                throw new ArgumentException(LocationErrors.xRangeError);
+
+            if (x > 10) 
+                throw new ArgumentException(LocationErrors.xRangeError);
+
+            this.X = x;
+        }
+
+        public void SetY(int y)
+        {
+            if (y < 1)
+                throw new ArgumentException(LocationErrors.yRangeError);
+
+            if (y > 10)
+                throw new ArgumentException(LocationErrors.yRangeError);
+
+            this.Y = y;
+        }
+
+        public static Location CreateRandom()
+        {
+
+            int x = _random.Next(1, 11);
+            int y = _random.Next(1, 11);
+
+            return new Location(x, y);
+        }
+
+
+        public int DistanceTo(Location destination)
+        {
+            int distanceX = Math.Abs(this.X - destination.X);
+            int distanceY = Math.Abs(this.Y - destination.Y);
+
+            return distanceX + distanceY;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return X;
+            yield return Y;
+        }
+    }
+}
---
+++ Tests/DeliveryApp.UnitTests/Domain/Model/SharedKernel/LocationShould.cs
@@ -0,0 +1,143 @@
+﻿using DeliveryApp.Core.Domain.SharedKernel;
+using FluentAssertions;
+using NSubstitute.ExceptionExtensions;
+using System;
+using Xunit;
+
+namespace DeliveryApp.UnitTests.Domain.Model.SharedKernel
+{
+    public class LocationShould
+    {
+
+        [Fact]
+        public void BeCorrectWhenParamsIsCorrectOnCreate()
+        {
+            //Arrange
+
+            //Act
+            var location = new Location(1, 5);
+
+            //Assert
+            location.Should().NotBeNull();
+            location.X.Should().Be(1);
+            location.Y.Should().Be(5);
+        }
+
+        [Fact]
+        public void BeEqualSameLocation()
+        {
+            //Arrange
+
+            //Act
+            var firstLocation = new Location(1, 5);
+            var secondLocation = new Location(1, 5);
+
+            //Assert
+            firstLocation.Should().Be(secondLocation);
+        }
+
+        [Fact]
+        public void BeNotEqualDifferentLocation()
+        {
+            //Arrange
+
+            //Act
+            var firstLocation = new Location(1, 5);
+            var secondLocation = new Location(1, 6);
+
+            //Assert
+            firstLocation.Should().NotBe(secondLocation);
+        }
+
+        [Theory]
+        [InlineData(11, 1)]
+        [InlineData(0, 10)]
+        [InlineData(-1, 3)]
+        public void ThrowExcceptionWhenXParamIsIncorrectAndYParamIsCorrectOnCreated(int x, int y)
+        {
+            //Arrange
+
+            //Act
+            Action act = () => new Location(x, y);
+
+            //Assert
+            act.Should().Throw<ArgumentException>().WithMessage("X should be between 1 and 10");
+        }
+
+        [Theory]
+        [InlineData(10, 0)]
+        [InlineData(4, -8)]
+        [InlineData(2, 11)]
+
+        public void ThrowExcceptionWhenYParamIsIncorrectAndXParamIsCorrectOnCreated(int x, int y)
+        {
+            //Arrange
+
+            //Act
+            Action act = () => new Location(x, y);
+
+            //Assert
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(11, 11)]
+        [InlineData(-1, -1)]
+        [InlineData(-2, 11)]
+        [InlineData(11, -5)]
+        public void ThrowExcceptionWhenParamsIsIncorrectOnCreated(int x, int y)
+        {
+            //Arrange
+
+            //Act
+            Action act = () => new Location(x, y);
+
+            //Assert
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [Theory]
+        [InlineData(2, 6, 4, 9, 5)]
+
+        public void ReturnCorrectStepsToDestionationAndBackward(int x1, int y1, int x2, int y2, int expectedDistance)
+        {
+            //Arrange
+
+            //Act
+            var startLocation = new Location(x1, y1);
+            var destination = new Location(x2, y2);
+
+            var backwardStartLocation = new Location(x2, y2);
+            var backwardDestination = new Location(x1, y1);
+
+            //Assert
+            startLocation.DistanceTo(destination).Should().Be(expectedDistance);
+            backwardStartLocation.DistanceTo(backwardDestination).Should().Be(expectedDistance);
+        }
+
+
+        [Fact]
+        public void BeCorrectWhenItsRandom()
+        {
+            //Arrange
+
+            //Act
+            var location = Location.CreateRandom();
+
+            //Assert
+            location.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void BeCorrectWhenItsMultipleRandom()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var location = Location.CreateRandom();
+
+                location.Should().NotBeNull();
+            }
+        }
+    }
+}
