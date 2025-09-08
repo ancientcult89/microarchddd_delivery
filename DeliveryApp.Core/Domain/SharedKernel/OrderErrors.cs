fab880c7d37995b58d26cafc2b1daf8d302c61b2
SP_Polyakov <sp_polyakov@dba.ooo>
2025-09-08 10:41:08 +0700

    ДЗ 05 - реализован доменный сервис диспетчеризации - реализована логика назначения ближайшего курьера на заказ - реализованы тесты сервиса диспетчеризации - ошибки агрегата заказов вынесены в SharedKernel

---
+++ DeliveryApp.Core/Domain/SharedKernel/OrderErrors.cs
@@ -0,0 +1,35 @@
+﻿using Primitives;
+
+namespace DeliveryApp.Core.Domain.SharedKernel
+{
+    internal class OrderErrors
+    {
+        public static Error OrderIsAlreadyAssigned(Guid? courierId)
+        {
+            if (!courierId.HasValue) throw new ArgumentException(courierId.ToString());
+            return new Error("order.is.already.assigned", $"Value is already assigned for {courierId.ToString()}");
+        }
+
+        public static Error CourierIsNeeded()
+        {
+            return new Error("courier.is.needed", $"Courier is needed");
+        }
+
+        public static Error OrderIsNotAssigned(Guid orderId)
+        {
+            if (orderId == Guid.Empty) throw new ArgumentException(orderId.ToString());
+            return new Error("order.is.not.assigned", $"Order {orderId.ToString()} is not assigned. Its cant complete");
+        }
+
+        public static Error OrderIsCompleted(Guid orderId)
+        {
+            if (orderId == Guid.Empty) throw new ArgumentException(orderId.ToString());
+            return new Error("order.is.completed", $"Order {orderId.ToString()} is already completed");
+        }
+
+        public static Error OrderIsNotExists()
+        {
+            return new Error("order.is.not.exists", $"Order is not exists");
+        }
+    }
+}
