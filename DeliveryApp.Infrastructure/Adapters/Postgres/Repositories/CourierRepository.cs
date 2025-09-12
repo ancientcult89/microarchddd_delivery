d5a714d48d8089f416fe54213f00254fcfe72f59
SP_Polyakov <sp_polyakov@dba.ooo>
2025-09-12 15:09:42 +0700

    - метод StoragePlace IsOccupied сделан открытым, дабы не дублировать логику проверки в тестах - добавлен класс утилит для теста, пока безсистемно, по мере разрастания нужно будет разложить по папочкам - реализован репозиторий CourierRepository, а так же тесты для него - перенесён из примера OrderRepository, ApplicationDbContext, UnitOfWork и конфигурации EFCore

---
+++ DeliveryApp.Infrastructure/Adapters/Postgres/Repositories/CourierRepository.cs
@@ -0,0 +1,46 @@
+﻿using CSharpFunctionalExtensions;
+using DeliveryApp.Core.Domain.Model.CourierAggregate;
+using DeliveryApp.Core.Ports;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeliveryApp.Infrastructure.Adapters.Postgres.Repositories
+{
+    public class CourierRepository : ICourierRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CourierRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+        public async Task AddAsync(Courier courier)
+        {
+            await _dbContext.Couriers.AddAsync(courier);
+        }
+
+        public async Task<Maybe<Courier>> GetAsync(Guid courierId)
+        {
+            var courier = await _dbContext
+                .Couriers
+                .SingleOrDefaultAsync(c => c.Id == courierId);
+            return courier;
+        }
+
+        public void Update(Courier courier)
+        {
+            _dbContext.Couriers.Update(courier);
+        }
+        public async Task<Maybe<List<Courier>>> GetAllFreeCouriers()
+        {
+            var couriersWithStorage = await _dbContext
+                .Couriers
+                .Include(c => c.StoragePlaces)
+                .ToListAsync();
+
+            var freeCouriers = couriersWithStorage
+                .Where(c => c.StoragePlaces.All(sp => !sp.IsOccupied()))
+                .ToList();
+
+            return freeCouriers;
+        }
+    }
+}
---
+++ TestUtils/TestModelCreator.cs
@@ -0,0 +1,28 @@
+﻿using DeliveryApp.Core.Domain.Model.CourierAggregate;
+using DeliveryApp.Core.Domain.Model.OrderAggrerate;
+using DeliveryApp.Core.Domain.SharedKernel;
+
+namespace TestUtils
+{
+    public static class TestModelCreator
+    {
+        public static Order CreateTestOrder()
+        {
+            var orderId = Guid.NewGuid();
+            var location = Location.Create(5, 5).Value;
+            var volume = 10;
+
+            return Order.Create(orderId, location, volume).Value;
+        }
+
+        public static Courier CreateTestCourier()
+        {
+            var courierId = Guid.NewGuid();
+            var name = $"Test Courier {courierId}";
+            var speed = 2;
+            var location = Location.CreateRandom().Value;
+
+            return Courier.Create(name, speed, location).Value;
+        }
+    }
+}
